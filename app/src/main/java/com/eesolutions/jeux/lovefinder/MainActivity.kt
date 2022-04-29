f16353ab4e4a3cb4823c84c1ff7165733365c404
Thang PHAM <phamthang37@yahoo.com>
2022-04-29 15:12:11 +0200

    Full screen, images, sound, fragment

--- app/src/main/java/com/eesolutions/jeux/lovefinder/MainActivity.kt
+++ app/src/main/java/com/eesolutions/jeux/lovefinder/MainActivity.kt
@@ -2,10 +2,17 @@ package com.eesolutions.jeux.lovefinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main)
+
     }
 }
